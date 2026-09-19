e3dbf157a2c34e72e3d09d7c346a14094499fc1f
Serena <serena@eadartrans.com>
2026-09-19 20:39:12 +0800

    debug(C3): send-completion-email 加 console.log 看 handler 是否被调用

--- netlify/functions/send-completion-email.js
+++ netlify/functions/send-completion-email.js
@@ -13,6 +13,7 @@ const { checkPreference } = require('./_shared/notifications');
 const nodemailer = require('nodemailer');
 
 exports.handler = async (event) => {
+  console.log('[send-completion-email] handler called, body:', event.body, 'authHeader present:', !!event.headers.authorization);
   const pre = preflight(event);
   if (pre) return pre;
 
@@ -99,7 +100,7 @@ exports.handler = async (event) => {
       html: tpl.html,
     });
 
-    console.log('send-completion-email sent:', info.messageId);
+    console.log('[send-completion-email] sent:', info.messageId, 'to:', tpl.to);
 
     return corsResponse(200, {
       success: true,
